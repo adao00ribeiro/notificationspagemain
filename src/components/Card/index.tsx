4418699cad92ac089170897dd261921c3fcd8023
Adão Ribeiro <adao-eduardo@hotmail.com>
2023-06-22 13:16:06 -0300

    att pnpm

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -41,15 +41,19 @@ export function Card(props: ICard) {
                 <div className={styles.containerAvatar}>
                     <Image src={props.urlAvatar} fill alt={"avatar"}></Image>
                 </div>
-                <div className={styles.containerRight}>
-                    <div className={styles.containerRight2}>
-                        <div className={styles.containerMain2}>
-                            <div className={styles.containerInfo}>
+                <div className={styles.containerInfo}>
+
+                    <div className={styles.containerInfoTop}>
+                        <div className={styles.groupInfo}>
+                            <div className={styles.groupInfoTop}>
                                 <span><Link className={styles.Link} href={""}>{props.name}</Link>
                                     {" " + props.text1 + " "}
-                                    <Link className={styles.Link} href={""}>{props.text2}</Link>
+                                    <Link className={styles.Link} href={""}>{props.text2}
+                                        <div ref={navRef} ></div>
+                                    </Link>
+
                                 </span>
-                                <div ref={navRef} ></div>
+
                             </div>
                             <div className={styles.containerDate}>
                                 <label htmlFor="">{props.date}</label>
@@ -61,11 +65,14 @@ export function Card(props: ICard) {
                             </div>
                         }
                     </div>
+
                     {props.EStatus === EStatus["MESSAGE"] &&
-                        <Link className={styles.containerMessage} href={""}>
-                            Hello, thanks for setting up the Chess Club. Ive been a member for a few weeks now and
-                            Im already having lots of fun and improving my game.
-                        </Link>
+                        <div className={styles.containerInfoBottom}>
+                            <Link href={""}>
+                                Hello, thanks for setting up the Chess Club. Ive been a member for a few weeks now and
+                                Im already having lots of fun and improving my game.
+                            </Link>
+                        </div>
                     }
                 </div>
 
